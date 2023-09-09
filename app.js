620a98cc3e5c8382475878c0d932232eaee2e5d8
Shahrukh-Rehman <shahrukhrehman321@gmail.com>
2023-09-10 03:25:02 +0500

    Question 1 to 5

---
+++ app.js
@@ -0,0 +1,78 @@
+//1
+// var a=10
+// document.write("Result:"+"<br>"+"The value of a is " + a +"<br>"+"--------------------------------------"+"<br>")
+// a=++a
+// document.write("<br>"+"The value of ++a is " + a +"<br>"+"Now The value of a is "+a+"<br>")
+// a=a++
+// document.write("<br>"+"The value of a++ is " + a++)
+// document.write("<br>"+"Now The value of a is " + a+"<br>")
+// a=--a
+// document.write("<br>"+"The value of --a is " + a)
+// document.write("<br>"+"Now The value of a is " + a+"<br>")
+// a=a--
+// document.write("<br>"+"The value of a-- is " + a--)
+// document.write("<br>"+"Now The value of a is " + a+"<br>")
+//2
+// var a=2,b=1;
+// result of --a is 1
+//result of --a - --b is 1
+//result of --a - --b + ++b is 2
+//result of --a - --b + b-- is 3
+// var result= --a - --b + ++b + b--;
+// document.write("a is "+a+"<br>"+"b is "+b+"<br>"+"result is "+ result)
+//3
+// var input= prompt("Write your name: ")
+// alert("Hello! " + input)
+//4
+// var table= Number( prompt("Write a table no which you want"))
+// if(table===0){
+// table=5
+// }
+// var counting = [1,2,3,4,5,6,7,8,9,10]
+// document.write("Table of " + table)
+// document.write("<br>");
+// document.write(table + "x" + counting[0] + "=" + table * counting[0])
+// document.write("<br>");
+// document.write(table + "x" + counting[1] + "=" + table * counting[1])
+// document.write ("<br>");
+// document.write(table + "x" + counting[2] + "=" + table * counting[2])
+// document.write("<br>");
+// document.write(table + "x" + counting[3] + "=" + table * counting[3])
+// document.write("<br>");
+// document.write(table + "x" + counting[4] + "=" + table * counting[4])
+// document.write("<br>");
+// document.write(table + "x" + counting[5] + "=" + table * counting[5])
+// document.write("<br>");
+// document.write(table + "x" + counting[6] + "=" + table * counting[6])
+// document.write("<br>");
+// document.write(table + "x" + counting[7] + "=" + table * counting[7])
+// document.write("<br>");
+// document.write(table + "x" + counting[8] + "=" + table * counting[8])
+// document.write("<br>");
+// document.write(table + "x" + counting[9] + "=" + table * counting[9])
+//5
+// var subjectOne=prompt("Write first subject")
+// var subjectTwo=prompt("Write second subject")
+// var subjectThree=prompt("Write third subject")
+// var totalMarks=100
+// var subjectOneMarks= Number (prompt("Write first subject obtained marks"))
+// var subjectTwoMarks= Number (prompt("Write second subject obtained marks"))
+// var subjectThreeMarks=Number (prompt("Write third subject obtained marks"))
+// var subjectOnePer= (subjectOneMarks/totalMarks)*100
+// var subjectTwoPer= (subjectTwoMarks/totalMarks)*100
+// var subjectThreePer= (subjectThreeMarks/totalMarks)*100
+// var totalSubjectMarks= totalMarks*3
+// var totalObtainedMarks= subjectOneMarks + subjectTwoMarks + subjectThreeMarks
+// var totalPercentage= (totalObtainedMarks/totalSubjectMarks)*100
+// document.write("Subject Total Marks Obtained Marks Percentage".bold())
+// document.write("<br>"+subjectOne + '&nbsp &nbsp &nbsp &nbsp &nbsp'+ totalMarks+'&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp'+subjectOneMarks+'&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp'+subjectOnePer+"%")
+// document.write("<br>"+subjectTwo + '&nbsp &nbsp &nbsp &nbsp &nbsp'+ totalMarks+'&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp'+subjectTwoMarks+'&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp'+subjectTwoPer+"%")
+// document.write("<br>"+subjectThree + '&nbsp &nbsp &nbsp &nbsp &nbsp'+ totalMarks+'&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp'+subjectThreeMarks+'&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp'+subjectThreePer+"%")
+// document.write("<br>"+'&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp'+totalSubjectMarks+'&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp'+totalObtainedMarks+'&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp'+totalPercentage+"%")
+
+
+
+
+
+
+
